a1fbab693c9e641c00136224b7f2a545a56d710f
ubu <ubu@ubu-VirtualBox.(none)>
2014-10-23 20:24:14 +0200

    msm8x60: Disable 2D GPU cores The 2D GPUs have been broken since cyanogen backported the newer kgsl drivers from CAF jb_2.6, as he forgot to fix the regulators for the 2D GPUs. No modern device has 2D GPUs, and nobody has noticed that they've been broken for a long time, so just remove them completely.

--- arch/arm/mach-msm/board-msm8x60_s7pro.c
+++ arch/arm/mach-msm/board-msm8x60_s7pro.c
@@ -5715,8 +5715,6 @@ static struct platform_device *rumi_sim_devices[] __initdata = {
 #endif
 	&msm_fb_device,
 	&msm_kgsl_3d0,
-	&msm_kgsl_2d0,
-	&msm_kgsl_2d1,
 #ifdef CONFIG_FB_MSM_LCDC_SAMSUNG_WSVGA
 	&lcdc_samsung_panel_device,
 #endif
@@ -6730,8 +6728,6 @@ static struct platform_device *surf_devices[] __initdata = {
 #endif
 	&msm_fb_device,
 	&msm_kgsl_3d0,
-	&msm_kgsl_2d0,
-	&msm_kgsl_2d1,
 #ifdef CONFIG_FB_MSM_LCDC_SAMSUNG_WSVGA
 	&lcdc_samsung_panel_device,
 #endif
@@ -12517,8 +12513,6 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 				ARRAY_SIZE(msm_rpmrs_levels)));
 	if (msm_xo_init())
 		pr_err("Failed to initialize XO votes\n");
-
-	msm8x60_check_2d_hardware();
 
 	/* Change SPM handling of core 1 if PMM 8160 is present. */
 	soc_platform_version = socinfo_get_platform_version();
