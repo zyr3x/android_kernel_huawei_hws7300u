39b6e1eb8e22b8cc5bca666ea92d3d7abd46cd37
ubu <ubu@ubu-VirtualBox.(none)>
2014-10-28 21:07:52 +0100

    mdp clock fix

--- arch/arm/mach-msm/clock-8x60.c
+++ arch/arm/mach-msm/clock-8x60.c
@@ -2426,7 +2426,7 @@ static struct rcg_clk mdp_clk = {
 		.dbg_name = "mdp_clk",
 		.ops = &clk_ops_rcg_8x60,
 		VDD_DIG_FMAX_MAP3(LOW,   85330000, NOMINAL, 200000000,
-				  HIGH, 228571000),
+				  HIGH, 266667000),
 		CLK_INIT(mdp_clk.c),
 		.depends = &mdp_axi_clk.c,
 	},
