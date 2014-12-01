f7a2ca1e4e90dc43074a68474773c8b45a7177bf
bart594 <ubu@ubu-VirtualBox.(none)>
2014-12-01 20:01:41 +0100

    cpufreq: intelliactive from faux123
    intelliactive is based on Latest Google Interactive cpufreq governor from
    Google.  The original Interactive provides smooth UI operations but suffers
    slight battery drain due to its aggressive policies.  Intelliactive is an
    improvement built on top of it.
    
    It has the following enhancements added:
    1. self-boost capability from input drivers (no need for PowerHAL assist)
    2. two phase scheduling (idle/busy phases to prevent from jumping directly to
       max freq
    3. Checks for offline cpus and short circuits some unnecessary checks to
       improve code execution paths
    4. removed trace calls
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -373,6 +373,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
 extern struct cpufreq_governor cpufreq_gov_lazy;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lazy)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intelliactive)
 #endif
 
 
