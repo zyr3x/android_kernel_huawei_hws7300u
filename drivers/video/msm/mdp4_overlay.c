1983de711617324d510c842b2034f9b874ab8802
bart594 <ubu@ubu-VirtualBox.(none)>
2014-12-05 23:24:52 +0100

    msmfb: partial fix hdmi blue screen

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -1793,7 +1793,8 @@ void mdp4_mixer_stage_commit(int mixer)
         if(data)
 	mdp4_mixer_blend_setup(mixer);
 
-	off = 0;
+        local_irq_save(flags);
+        off = 0;
 	if (data != ctrl->mixer_cfg[mixer]) {
 		ctrl->mixer_cfg[mixer] = data;
 		if (mixer >= MDP4_MIXER2) {
@@ -1810,7 +1811,6 @@ void mdp4_mixer_stage_commit(int mixer)
 				mixer, data, ctrl->flush[mixer], current->pid);
 	}
 
-	local_irq_save(flags);
 	if (off)
 		outpdw(MDP_BASE + off, data);
 
